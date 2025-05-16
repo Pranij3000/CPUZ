a235131a85051e33d72740e2d159a04255a88c6e
Pranij Manandhar <pranij.makura@gmail.com>
2025-05-17 00:51:36 +0545

    initial commit

---
+++ client/src/page-templates/App.jsx
@@ -0,0 +1,52 @@
+export default function App() {
+  return (
+    <>
+      <div className="section-title">
+        <h2>
+          <span>CPUZ: </span>
+          The best marketplace
+        </h2>
+      </div>
+
+      <h1>CPUZ</h1>
+      <h2>CPUZ</h2>
+      <h3>CPUZ</h3>
+      <h4>CPUZ</h4>
+      <h5>CPUZ</h5>
+      <h6>CPUZ</h6>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta, laborum. Quidem amet ducimus nisi eaque dolore voluptates magni quo perferendis natus incidunt. Omnis ex incidunt aut. Libero, vel ipsum deserunt quis quaerat totam perferendis necessitatibus culpa repellat ratione cum iure vero consectetur maiores facilis eum, tenetur delectus cumque cupiditate perspiciatis ipsam! Nemo animi reprehenderit aut aspernatur excepturi possimus, deleniti repellat natus necessitatibus ipsa repudiandae sunt facilis nisi eius voluptates voluptatibus doloremque delectus unde quae sapiente adipisci atque molestiae incidunt. Deserunt illum quis pariatur sint consequuntur aut voluptas perferendis sunt tempora qui assumenda quos laboriosam cupiditate, quasi dolore id delectus magni ratione veritatis rerum non dicta consequatur debitis saepe! Atque sunt molestiae eveniet quas illum. Deleniti distinctio atque aliquam, error optio ea non doloremque ipsa, reprehenderit perspiciatis rem sapiente reiciendis veritatis eos, laboriosam sint tenetur obcaecati quas harum impedit. Explicabo officiis, iusto quidem aperiam rem distinctio voluptas ipsum unde nihil? Esse, voluptatem maxime eligendi maiores veritatis officia aspernatur possimus unde incidunt quidem delectus ratione. Similique maiores praesentium iure atque pariatur voluptatibus hic beatae? Reprehenderit, eum sit nisi assumenda expedita aspernatur! Praesentium aliquid explicabo fuga consectetur ex esse distinctio rerum cum, tempore iste fugiat laboriosam facilis? Aliquid temporibus excepturi ab labore praesentium.</p>
+      <div className="styled-ul">
+        <ul>
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem inventore, odio laboriosam repudiandae aperiam quos voluptates non nesciunt cupiditate, doloribus eligendi commodi consequatur voluptatum facilis! Dolores et neque hic molestias non asperiores nulla ipsum numquam saepe eveniet amet ipsa porro blanditiis recusandae, perspiciatis repudiandae laboriosam nihil ducimus! Eius incidunt omnis deleniti qui, totam aspernatur facere illum maiores veritatis dicta quis magni iste itaque, at ullam tempore laborum similique distinctio optio vero debitis placeat dolore animi neque. Quidem voluptatem corporis ipsum aliquid cumque numquam, perspiciatis, labore facere voluptates at quod quia, neque enim alias quisquam excepturi quo doloremque repellendus ea? Magnam repudiandae, quam nulla deleniti recusandae non vel voluptatum assumenda delectus facilis dolorem saepe autem perspiciatis quod fugit, laborum, doloremque rerum. Nesciunt, libero quasi illum cum maxime quia esse non facere, ipsum repellendus doloribus minus aperiam quaerat fugit placeat itaque quisquam consequatur optio quis laudantium modi! Ullam aut expedita saepe reprehenderit debitis! Enim ratione quod ea sunt fugit ipsum deleniti id tempora modi, nemo, exercitationem ex commodi, consequuntur magnam sit! Sed iusto ullam perferendis placeat ut ab ratione laudantium repellat quia voluptatem reprehenderit recusandae ad esse cupiditate nostrum tenetur nobis, quos, ipsa nulla, animi alias? Consectetur eveniet ipsam omnis optio quod?</li>
+          <li>assumenda</li>
+          <li>asdsad</li>
+        </ul>
+      </div>
+      <strong>CPUZ</strong>
+      <div className="styled-table">
+        <table>
+          <tr>
+            <th>CPUZ</th>
+            <th>CPUZ</th>
+          </tr>
+          <tr>
+            <td>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe, in?</td>
+            <td>CPUZ</td>
+          </tr>
+          <tr>
+            <td>CPUZ</td>
+            <td>CPUZ</td>
+          </tr>
+          <tr>
+            <td>CPUZ</td>
+            <td>CPUZ</td>
+          </tr>
+          <tr>
+            <td>CPUZ</td>
+            <td>CPUZ</td>
+          </tr>
+        </table>
+      </div>
+    </>
+  );
+}
