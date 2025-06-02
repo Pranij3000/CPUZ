362d783a6483a8b9d44a4b944a54f2083fdf7700
Pranij Manandhar <pranij.makura@gmail.com>
2025-06-02 20:22:41 +0545

    error page started

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,7 +1,18 @@
+import "./Error.scss";
 export default function ErrorPage() {
-  return (
-    <>
-      <h1>Error</h1>
-    </>
-  );
+	return (
+		<>
+			<section className="error-page py-64">
+				<div className="container">
+					<div className="error-page-wrapper d-flex align-items-end">
+						<div className="title">
+							<h1>
+								<span className="text-white">Page not Found</span>
+							</h1>
+						</div>
+					</div>
+				</div>
+			</section>
+		</>
+	);
 }
