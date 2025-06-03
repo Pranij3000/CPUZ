3e30804680112f2c38b8be4ab60b8956bd1508d7
Pranij Manandhar <pranij.makura@gmail.com>
2025-06-04 00:51:56 +0545

    forms completed

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,7 +1,10 @@
 import GetInTouch from "../sections/GetInTouch";
+import BannerSimple from "../sections/BannerSimple";
+
 export default function Contact() {
   return (
     <>
+      <BannerSimple />
       <GetInTouch />
     </>
   );
---
+++ client/src/sections/BannerSimple.jsx
@@ -0,0 +1,12 @@
+import "./BannerSimple.scss";
+export default function BannerSimple() {
+  return (
+    <section className="banner-simple bg-main-gradient pt-96 pb-48">
+      <div className="container">
+        <div className="title text-center">
+          <h1>Contact US</h1>
+        </div>
+      </div>
+    </section>
+  );
+}
